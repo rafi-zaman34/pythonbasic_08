6777b7b80f70858d175fc2fe7c7b90b295067fe1
rafi-zaman34 <rafiuzzaman@gmail.com>
2022-08-26 12:17:30 +0600

    New listing

---
+++ variables_demo/list.py
@@ -0,0 +1,19 @@
+# student1='devid'
+# mail= 'mail@gmail.com'
+# phone= '12345678'
+# address= 'dhaka'
+#
+# student2='devid'
+# mail= 'mail@gmail.com'
+# phone= '12345678'
+# address= 'dhaka'
+#
+# student2 = ['david','mail@gmail.com','123456','dhaka']
+# student2 = ['smith','smith@gmail.com','00123456','khulna']
+#
+# print(student1[0]
+
+
+
+guest = ['g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'g9', 'g10']
+print("guest list for luncheon")
---
+++ variables_demo/list_2.py
@@ -0,0 +1,12 @@
+guest = ['g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'g9', 'g10']
+
+print("guest list for lunch")
+
+lunch = guest[0:4]
+dinner = guest[5:9]
+
+print(lunch)
+
+print("guest list for dinner")
+
+print(dinner)
