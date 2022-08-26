b6e050c71715233158f9b63dda21d287c63ec2c4
rafi-zaman34 <rafiuzzaman@gmail.com>
2022-08-26 12:41:28 +0600

    listing

--- variables_demo/list_2.py
+++ variables_demo/list_2.py
@@ -1,12 +1,30 @@
-guest = ['g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'g9', 'g10']
+# guest = ['g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'g9', 'g10']
+#
+# print("guest list for lunch")
+#
+# lunch = guest[0:4]
+# dinner = guest[5:9]
+#
+# print(lunch)
+#
+# print("guest list for dinner")
+#
+# print(dinner)
 
-print("guest list for lunch")
+number = [12, 3, 56, 8, 90]
 
-lunch = guest[0:4]
-dinner = guest[5:9]
+number.pop(1)
+print(number)
 
-print(lunch)
+number.reverse()
+print(number)
 
-print("guest list for dinner")
+number.sort()
+print(number)
 
-print(dinner)
+number.append(100)
+print(number)
+
+number.clear()
+print(number)
+
